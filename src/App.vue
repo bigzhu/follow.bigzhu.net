f0ff621b1e689c03d2e95c693681032f8c7578b6
bigzhu <vermiliondun@gmail.com>
2017-04-02 22:58:12 +0800

    简单的样式调整

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
 <template>
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
-    <router-view></router-view>
+    <q-layout>
+      <div slot="header" class="toolbar dark inverted">
+
+        <!-- opens drawer using its ref -->
+        <button
+          class="hide-on-drawer-visible"
+          @click="$refs.drawer.open()"
+          >
+          <i>menu</i>
+        </button>
+        <q-toolbar-title :padding="2">
+          <img class="logo-img" src="./assets/logo.svg">
+          Follow Center
+        </q-toolbar-title>
+        <q-search placeholder="搜索" class="white toolbar-search" v-model="searchModel" ></q-search>
+      </div>
+      <div slot="header" class="toolbar dark inverted">
+        <button>
+          寻他
+        </button>
+        <button>
+          传记
+        </button>
+      </div>
+      <router-view class="layout-view"></router-view>
+    </q-layout>
   </div>
 </template>
 
 <script>
-/*
- * Root component
- */
-export default {}
+  /*
+  * Root component
+  */
+  export default {}
 </script>
 
-<style></style>
+<style>
+  html {
+    font-size: 14px;
+  }
+  .toolbar-title > div {
+    padding-top: .9rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+  .logo-img {
+    vertical-align: middle;
+    width: 2.5rem;
+  }
+  .toolbar-search {
+    width: auto;
+  }
+  .q-search.white .q-search-input {
+    background: white;
+    box-shadow: none;
+  }
+  body.desktop .q-search.white .q-search-input:hover {
+    background: none;
+  }
+  body.desktop .q-search.white .q-search-input:focus {
+    color: rgba(0,0,0,.8);
+  }
+
+</style>
