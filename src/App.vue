f9623615169cc3030d2abc1c48df6fbb289cd50e
bigzhu <vermiliondun@gmail.com>
2017-04-03 22:28:31 +0800

    把 Header 拆出来

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,8 @@
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
     <q-layout>
-      <div slot="header">
-      <div slot="header" class="toolbar dark inverted menu-bz header-one-bz">
-        <!-- opens drawer using its ref -->
-        <button
-          class="hide-on-drawer-visible"
-          @click="$refs.drawer.open()"
-          >
-          <i>menu</i>
-        </button>
-        <q-toolbar-title :padding="2">
-          <img class="logo-img" src="./assets/logo.svg">
-          Follow Center
-        </q-toolbar-title>
-        <q-search v-model="search_value" :debounce="600" placeholder="搜索" class="white toolbar-search"  ></q-search>
-
-        <a class="menu-item login-bz" href="javascript:;">
-          登录
-        </a>
-      </div>
-      <div slot="header" class="toolbar dark inverted menu-bz toolbar-item">
-        <a class="menu-item" href="javascript:;">
-          寻他
-        </a>
-        <a class="menu-item" href="javascript:;">
-          传记
-        </a>
-      </div>
-      </div>
+      <HeaderBz slot="header">
+      </HeaderBz>
       <div class="layout-view">
         <router-view></router-view>
       </div>
@@ -42,8 +16,12 @@
   * Root component
   */
   import store from './store'
+  import HeaderBz from './components/HeaderBz'
   export default {
     store,
+    components: {
+      HeaderBz
+    },
     data () {
       return {
         search_value: ''
@@ -53,33 +31,6 @@
 </script>
 
 <style>
-  .menu-item.login-bz {
-    padding: 22px 16px;
-  }
-  .header-one-bz {
-    height: 65px;
-  }
-  .layout-header {
-    box-shadow: none;
-  }
-  .toolbar.menu-bz {
-    padding: inherit;
-  }
-  .toolbar-item {
-    border-top: .5px solid rgba(0,0,0,.05);
-    justify-content: flex-start;
-    box-shadow: 1px 1px 1px rgba(0,0,0,.09);
-  }
-
-  .menu-item:hover {
-    background: rgba(0,0,0,.03);
-    color: rgba(0,0,0,.7);
-  }
-  .menu-item {
-    padding: 1em 2em;
-    color: rgba(0,0,0,.5);
-  }
-
   html {
     font-size: 14px;
     font-family: Lato,arial,Microsoft YaHei,"sans-serif";
@@ -87,27 +38,5 @@
     font-weight: 400;
     background-color: #fafafa;
     color: rgba(0,0,0,.76);
-  }
-  .toolbar-title > div {
-    padding: .93rem;
-    font-size: 1.5rem;
-    font-weight: 600;
-  }
-  .logo-img {
-    vertical-align: middle;
-    width: 2.5rem;
-  }
-  .toolbar-search {
-    width: auto;
-  }
-  .q-search.white .q-search-input {
-    background: white;
-    box-shadow: none;
-  }
-  body.desktop .q-search.white .q-search-input:hover {
-    background: none;
-  }
-  body.desktop .q-search.white .q-search-input:focus {
-    color: rgba(0,0,0,.8);
   }
 </style>
