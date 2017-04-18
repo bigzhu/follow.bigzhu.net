1ab6e818a870ad4ea042b7cc9ba78fcf979810f0
bigzhu <vermiliondun@gmail.com>
2017-04-19 00:56:44 +0800

    基本搞定右边样式

--- src/components/RightInfo.vue
+++ src/components/RightInfo.vue
@@ -14,17 +14,17 @@
       <a class="footer-element" href="/about.html">{{ $t("RightInfo.about") }}</a>
       <a class="footer-element" href="http://bigzhu.lorstone.com">{{ $t("RightInfo.blog") }}</a>
       <router-link class="footer-element" :to="{ name: 'ChangeLog'}">{{ $t("RightInfo.changelog") }}</router-link>
-
     </div>
-      <q-select
-        type="list"
-        v-model="lang"
-        :options="lang_options">
-      </q-select>
-    <div class="footer-content">
+
+    <div class="footer">
       <span class="footer-element">{{ $t("RightInfo.register") }}：{{registered_count}}</span>
       <span>© 2017 Follow Center</span>
     </div>
+    <q-select
+      type="list"
+      v-model="lang"
+      :options="lang_options">
+    </q-select>
   </div>
 </template>
 
@@ -82,6 +82,26 @@
 </script>
 
 <style scoped>
+  .footer {
+    padding: 1rem 0;
+    border-top: 1px solid #eee;
+    display: flex;
+  }
+  .footer > * {
+    flex: 1;
+  }
+  .q-picker-textfield.textfield:focus {
+    border-bottom: none; 
+  }
+  .q-picker-textfield.textfield:hover {
+    border-bottom: none; 
+  }
+  .q-picker-textfield.textfield {
+    border-bottom: none; 
+  }
+  .q-popover list[style] {
+    min-width: initial;
+  }
   .about {
     margin-top: 3rem;
     padding: 1rem 0;
