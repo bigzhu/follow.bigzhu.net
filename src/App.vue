d5561822b36748d93c12875e0a62ea23120a71ca
bigzhu <vermiliondun@gmail.com>
2017-07-08 10:00:33 +0800

    加入 layout

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,132 @@
 <template>
   <div id="q-app">
-    <q-layout ref="layout" view="hHr LpR lFf" :right-breakpoint="1100">
-      <!-- Header -->
-      <q-toolbar slot="header">
-        <q-btn flat @click="$refs.layout.toggleLeft()">
-          <q-icon name="menu" />
-          </q-btn>
-          <q-toolbar-title>
-            Layout Header
-            <span slot="subtitle">Optional subtitle</span>
-          </q-toolbar-title>
-          <q-btn flat @click="$refs.layout.toggleRight()">
-            <q-icon name="menu" />
-            </q-btn>
-          </q-toolbar>
-          <!-- Navigation -->
-          <q-tabs slot="navigation">
-            <q-route-tab slot="title" icon="view_quilt" to="/test-layout/about" replace hide="icon" label="About" />
-              <q-route-tab slot="title" icon="view_day" to="/test-layout/toolbar" replace hide="icon" label="Toolbar" />
-                <q-route-tab slot="title" icon="view_day" to="/test-layout/tabs" replace label="Tabs" />
-                  <q-route-tab slot="title" icon="input" to="/test-layout/drawer" replace label="Drawer" />
-                  </q-tabs>
-                  <!-- Left Side Panel -->
-                  <div slot="left">
-                    <q-list no-border link inset-separator>
-                      <q-list-header>Essential Links</q-list-header>
-                      <q-side-link item to="/docs">
-                        <q-item-side icon="school"></q-item-side>
-                        <q-item-main label="Docs" sublabel="quasar-framework.org" />
-                        </q-side-link>
-                        <q-side-link item to="/forum">
-                          <q-item-side icon="record_voice_over" />
-                            <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-                            </q-side-link>
-                            <q-side-link item to="/chat">
-                              <q-item-side icon="chat" />
-                                <q-item-main label="Gitter Channel" sublabel="Quasar Lobby" />
-                                </q-side-link>
-                                <q-side-link item to="/twitter">
-                                  <q-item-side icon="rss feed" />
-                                    <q-item-main label="Twitter" sublabel="@quasarframework" />
-                                    </q-side-link>
-                                  </q-list>
-                                </div>
-                                <!-- Right Side Panel -->
-                                <div slot="right">
-                                  Right Side of Layout
-                                </div>
-                                <!-- sub-routes get injected here: -->
-                                <router-view></router-view>
-                                <!-- Footer -->
-                                <q-toolbar slot="footer">
-                                  <q-toolbar-title>
-                                    Layout Footer
-                                  </q-toolbar-title>
-                                </q-toolbar>
-                              </q-layout>
-                            </div>
-                          </template>
+  <q-layout
+    ref="layout"
+    :view="layoutStore.view"
+    :left-breakpoint="layoutStore.leftBreakpoint"
+    :right-breakpoint="layoutStore.rightBreakpoint"
+    :reveal="layoutStore.reveal"
+  >
+    <q-toolbar slot="header">
+      <q-btn flat @click="$refs.layout.toggleLeft()">
+        <q-icon name="menu" />
+      </q-btn>
+      <q-toolbar-title>
+        Quasar Layout
+        <span slot="subtitle">Empowering your app</span>
+      </q-toolbar-title>
+      <q-btn class="within-iframe-hide" flat @click="$router.replace('/showcase')" style="margin-right: 15px">
+        <q-icon name="keyboard_arrow_left" />
+        Go back
+      </q-btn>
+      <q-btn flat @click="$refs.layout.toggleRight()">
+        <q-icon name="menu" />
+      </q-btn>
+    </q-toolbar>
 
-                          <script>
-                            /*
-                            * Root component
-                            */
-                            export default {}
-                          </script>
+    <q-tabs slot="navigation" v-if="!layoutStore.hideTabs">
+      <q-route-tab slot="title" icon="play_circle_outline" to="/showcase/layout/play-with-layout" replace label="Play with Layout" />
+      <q-route-tab slot="title" icon="view_array" to="/showcase/layout/drawer-panels" replace label="Drawer Panels" />
+      <q-route-tab slot="title" icon="pin_drop" to="/showcase/layout/fixed-positioning" replace label="Fixed Positioning" />
+      <q-route-tab slot="title" icon="play_for_work" to="/showcase/layout/floating-action-button" replace label="Floating Action Button" />
+    </q-tabs>
 
-                          <style></style>
+    <q-scroll-area slot="left" style="width: 100%; height: 100%">
+      <q-list-header>Left Panel</q-list-header>
+      <q-side-link item to="/showcase/layout/play-with-layout">
+        <q-item-side icon="account circle" />
+        <q-item-main label="Play with Layout" sublabel="Learn more about it" />
+        <q-item-side right icon="thumb_up" />
+      </q-side-link>
+      <q-side-link item to="/showcase/layout/drawer-panels">
+        <q-item-side icon="view_array" />
+        <q-item-main label="Drawer Panels" sublabel="Layout left/right sides" />
+      </q-side-link>
+      <q-side-link item to="/showcase/layout/fixed-positioning">
+        <q-item-side icon="pin_drop" />
+        <q-item-main label="Fixed Positioning" sublabel="...on a Layout" />
+      </q-side-link>
+      <q-side-link item to="/showcase/layout/floating-action-button">
+        <q-item-side icon="play_for_work" />
+        <q-item-main label="Floating Action Button" sublabel="For Page actions" />
+      </q-side-link>
+
+      <div v-if="layoutStore.leftScroll" style="padding: 25px 16px 16px;">
+        <p class="caption" v-for="n in 50">
+          <em>Left Panel has intended scroll</em>
+        </p>
+      </div>
+    </q-scroll-area>
+
+    <q-scroll-area slot="right" style="width: 100%; height: 100%">
+      <q-list-header>Right Panel</q-list-header>
+      <div v-if="layoutStore.rightScroll" style="padding: 25px 16px 16px;">
+        <p class="caption" v-for="n in 50">
+          <em>Right Panel has intended scroll</em>
+        </p>
+      </div>
+    </q-scroll-area>
+
+    <router-view />
+
+    <q-toolbar slot="footer">
+      <q-toolbar-title>
+        Footer
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-layout>
+  </div>
+</template>
+
+<script>
+  /*
+   * Root component
+   */
+  import {
+    QLayout,
+    QToolbar,
+    QToolbarTitle,
+    QSearch,
+    QTabs,
+    QRouteTab,
+    QBtn,
+    QIcon,
+    QItemSide,
+    QItemMain,
+    QSideLink,
+    QListHeader,
+    QScrollArea
+  } from 'quasar'
+  export default {
+    components: {
+      QLayout,
+      QToolbar,
+      QToolbarTitle,
+      QSearch,
+      QTabs,
+      QRouteTab,
+      QBtn,
+      QIcon,
+      QItemSide,
+      QItemMain,
+      QSideLink,
+      QListHeader,
+      QScrollArea
+    },
+    data() {
+      return {
+        layoutStore: {
+          view: 'lHh LpR lFf',
+          reveal: false,
+          leftScroll: true,
+          rightScroll: true,
+          leftBreakpoint: 996,
+          rightBreakpoint: 1200,
+          hideTabs: false
+        }
+      }
+    }
+  }
+</script>
+
+<style></style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,62 +1,9 @@
 <template>
-  <q-layout
-    ref="layout"
-    view="lHh Lpr fff"
-    :left-class="{'bg-grey-2': true}"
-  >
-    <q-toolbar slot="header" class="glossy">
-      <q-btn
-        flat
-        @click="$refs.layout.toggleLeft()"
-      >
-        <q-icon name="menu" />
-      </q-btn>
-
-      <q-toolbar-title>
-        Quasar App
-        <div slot="subtitle">Running on Quasar v{{$q.version}}</div>
-      </q-toolbar-title>
-    </q-toolbar>
-
-    <div slot="left">
-      <!--
-        Use <q-side-link> component 
-        instead of <q-item> for 
-        internal vue-router navigation
-      -->
-
-      <q-list no-border link inset-delimiter>
-        <q-list-header>Essential Links</q-list-header>
-        <q-item @click="launch('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        </q-item>
-        <q-item @click="launch('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click="launch('https://gitter.im/quasarframework/Lobby')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Gitter Channel" sublabel="Quasar Lobby" />
-        </q-item>
-        <q-item @click="launch('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
-        </q-item>
-      </q-list>
-    </div>
-
-    <!--
-      Replace following <div> with
-      <router-view /> component
-      if using subRoutes
-    -->
     <div class="layout-padding logo-container non-selectable no-pointer-events">
       <div class="logo" :style="position">
         <img src="~assets/quasar-logo-full.svg">
       </div>
     </div>
-  </q-layout>
 </template>
 
 <script>
