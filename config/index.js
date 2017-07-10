81b7ad7b43c7bf915c68afed29d12ed0b1ed8cb0
bigzhu <vermiliondun@gmail.com>
2017-07-10 10:27:03 +0800

    加入proxy

--- config/index.js
+++ config/index.js
@@ -45,7 +45,16 @@ module.exports = {
     // Proxy your API if using any.
     // Also see /build/script.dev.js and search for "proxy api requests"
     // https://github.com/chimurai/http-proxy-middleware
-    proxyTable: {}
+    proxyTable: {
+      '/p': {
+        target: 'https://follow.center',
+        changeOrigin: true
+      },
+      '/api_': {
+        target: 'https://follow.center',
+        changeOrigin: true
+      }
+    }
   }
 }
 
