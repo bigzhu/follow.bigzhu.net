df71fb9f603d4b91d99b1ccf7138a72aae523eec
bigzhu <vermiliondun@gmail.com>
2017-07-18 16:51:07 +0800

    let pop look good

--- src/components/AddGodButton.vue
+++ src/components/AddGodButton.vue
@@ -5,24 +5,25 @@
     </q-fixed-position>
 
     <q-modal ref="confirm_refalsh_message">
-      <div class="card">
-        <div class="card-content card-force-top-padding">
-          <div class="floating-label">
-            <input @keyup.13="add" v-model="god_name" required class="full-width">
-            <label>{{$t('AddGod.example')}}</label>
-          </div>
-        </div>
-        <div class="card-actions card-no-top-padding space-between-bz">
-          <button class="primary" @click="add">好的</button>
-          <button class="light" @click="$refs.confirm_refalsh_message.close()">取消</button>
-        </div>
-      </div>
+      <q-card class="card">
+        <q-card-main>
+          <q-input @keyup.13="add" v-model="god_name" required :float-label="$t('AddGod.example')" />
+        </q-card-main>
+        <q-card-actions align="around">
+          <q-btn color="secondary" @click="add">好的</q-btn>
+          <q-btn class="light" @click="$refs.confirm_refalsh_message.close()">取消</q-btn>
+        </q-card-actions>
+      </q-card>
     </q-modal>
   </div>
 </template>
 
 <script>
   import {
+    QInput,
+    QCardActions,
+    QCardMain,
+    QCard,
     QBtn,
     QFixedPosition,
     QModal
@@ -30,6 +31,10 @@
   export default {
     props: [],
     components: {
+      QInput,
+      QCardActions,
+      QCardMain,
+      QCard,
       QBtn,
       QFixedPosition,
       QModal
@@ -52,25 +57,13 @@
   }
 </script>
 
-<style scoped>
-  .card-actions>button {
-    margin-left: 1rem;
-    margin-right: 1rem;
-  }
-
-  .card-actions>.primary {
-    padding-right: 1rem;
-  }
-
-  .light {
-    margin-left: 10%;
-  }
-
-  .card {
-    margin-bottom: 0;
+<style lang="stylus" scoped>
+  .q-card {
+    margin: 0;
   }
 
   .fixed-bottom-right {
-    bottom: 1rem;
+    bottom: 1rem
+    z-index 9999
   }
 </style>
