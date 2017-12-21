986741b97416daa201f32f2907d0254cd577e1e7
bigzhu <vermiliondun@gmail.com>
2017-12-21 12:44:06 +0800

    居中

--- src/components/Gold.vue
+++ src/components/Gold.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="layout-padding">
-    <q-input v-model="gold_conf.last_reverse_max" type="number" float-label="Last reverse max" />
-    <q-input v-model="gold_conf.atr" type="number" float-label="ATR" />
-    <q-radio v-model="gold_conf.oper" val="buy" label="Buy" />
-    <q-radio v-model="gold_conf.oper" val="sell" label="Sell" />
-    <q-input v-model="gold_conf.max" type="number" float-label="Max" />
+  <div class="layout-padding row justify-center">
+    <div style="width: 500px; max-width: 90vw;">
 
-    <q-btn icon="done" @click="done">Submit</q-btn>
-    <br>
-    <br>
-    <table class="q-table cell-separator bordered striped highlight">
-      <thead>
-        <tr>
-          <th class="text-left">{{gold_conf.oper}}</th>
-          <th class="text-right">stop</th>
-          <th class="text-right">reverse</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="i in trade.intervals">
-          <td class="text-left">{{i}}</td>
-          <td class="text-right">{{trade.stop}}</td>
-          <td class="text-right">{{trade.reverse}}</td>
-        </tr>
-      </tbody>
-    </table>
+      <q-input v-model="gold_conf.last_reverse_max" type="number" float-label="Last reverse max" />
+      <q-input v-model="gold_conf.atr" type="number" float-label="ATR" />
+      <q-radio v-model="gold_conf.oper" val="buy" label="Buy" />
+      <q-radio v-model="gold_conf.oper" val="sell" label="Sell" />
+      <q-input v-model="gold_conf.max" type="number" float-label="Max" />
+
+      <q-btn icon="done" @click="done">Submit</q-btn>
+      <br>
+      <br>
+      <table class="q-table cell-separator bordered striped highlight">
+        <thead>
+          <tr>
+            <th class="text-left">{{gold_conf.oper}}</th>
+            <th class="text-right">stop</th>
+            <th class="text-right">reverse</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in trade.intervals">
+            <td class="text-left">{{i}}</td>
+            <td class="text-right">{{trade.stop}}</td>
+            <td class="text-right">{{trade.reverse}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
