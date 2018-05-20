59cf02230b12827ec933c3a65be3de2e4f7c6d51
bigzhu <vermiliondun@gmail.com>
2018-05-20 22:08:11 +0800

    剔除无用的函数

--- src/components/MessageContent.vue
+++ src/components/MessageContent.vue
@@ -13,51 +13,12 @@
 </template>
 
 <script>
-  import _ from 'lodash'
   import myautolinker from '../functions/myautolinker'
   import Proxy from './Proxy'
   export default {
     mixins: [Proxy],
     props: ['message'],
-    data: function() {
-      return {}
-    },
     computed: {
-      medias: function() {
-        if (this.message.extended_entities) {
-          return _.map(this.message.extended_entities.media,
-            function(d) {
-              // var height, img_height, img_url, img_width, t
-              var img_url = d.media_url_https + ':orig'
-              var videos = []
-              var type = ''
-              if (d.video_info) {
-                // 找出类型是mp4,并且像素最好的
-                // videos = [d.video_info.variants[0]]
-                videos = _.filter(d.video_info.variants, {
-                  content_type: 'video/mp4'
-                })
-                if (videos.length > 1) {
-                  let video = _.maxBy(videos, function(v) {
-                    return v.bitrate
-                  })
-                  videos = [video] // 还是封成 list ，为遵循多视频考虑
-                }
-
-                if (d.type === 'animated_gif') {
-                  type = 'gif'
-                }
-              }
-              var t = {
-                img_url: img_url,
-                videos: videos,
-                type: type
-              }
-              return t
-            }
-          )
-        }
-      },
       text: function() {
         return myautolinker(this.message.text, this.message.social)
       }
