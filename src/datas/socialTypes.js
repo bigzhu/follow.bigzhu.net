bde026316febffb15c88486315e489c6aae75131
bigzhu <vermiliondun@gmail.com>
2019-07-09 19:47:05 +0800

    依赖的 styl 文件, 稍后再来修改

---
+++ src/datas/socialTypes.js
@@ -0,0 +1,23 @@
+export default [{
+    social_type: 'twitter',
+    is_edit: false
+  },
+  {
+    social_type: 'instagram',
+    is_edit: false
+  },
+  {
+    social_type: 'tumblr',
+    is_edit: false
+  }
+  /*
+  {
+    social_type: 'facebook',
+    is_edit: false
+  },
+  {
+    social_type: 'github',
+    is_edit: false
+  }
+  */
+]
