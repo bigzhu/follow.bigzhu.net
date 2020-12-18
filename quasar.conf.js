7f8018fdf34ef1ea7502265e076b881991002f75
bigzhu <vermiliondun@gmail.com>
2020-12-18 11:42:50 +0800

    proxy medias

--- quasar.conf.js
+++ quasar.conf.js
@@ -92,7 +92,11 @@ module.exports = function(ctx) {
     devServer: {
       proxy: {
         "/api": {
-          target: "http://127.0.0.1:8000",
+          target: "http://127.0.0.1:3003",
+          changeOrigin: true
+        },
+        "/medias": {
+          target: "https://follow.bigzhu.net",
           changeOrigin: true
         }
       },
